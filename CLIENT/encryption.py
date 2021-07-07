4be160e0d82a1ddf8214ac49b499eedde794021c
nhd911 <nhd.a1.chel@gmail.com>
2021-07-07 22:45:10 +0700

    duong add encrypt

---
+++ CLIENT/encryption.py
@@ -0,0 +1,22 @@
+from Crypto.Cipher import AES 
+import hashlib
+from Crypto import Random
+import base64
+
+
+class AESEncryption:
+    def __init__(self, password, vector): # vector here is base64
+        self.PASSWORD = password
+        self.KEY = hashlib.sha256(self.PASSWORD).digest() # generating a 32 bytes key 
+        self.IV = base64.b64decode(vector) # initialization vector, we need to make this random. It can be shared in plain text, it's not secret
+
+        self.MODE = AES.MODE_CFB # automatic padding mode (?)
+
+    def generate_cipher(self):
+        return AES.new(self.KEY, self.MODE, IV = self.IV)
+
+
+class DiffieHellman:
+    def __init__(self, sharedBase, sharedPrime):
+        self.sharedBase = sharedBase
+        self.sharedPrime = sharedPrime
